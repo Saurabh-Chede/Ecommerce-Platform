19757a2da6b4326c7c00086ac870f996257deef7
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-02 14:20:43 +0530

    feature added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,13 +30,22 @@ function Navbar({ cart, decreaseQuantity, increaseQuantity, totalAmount }) {
                 </button>
 
                 {!showcart && (
-                    <div className="absolute flex gap-10 right-0 mt-2 max-w-[700px] overflow-clip bg-yellow-100 shadow-lg rounded p-5 z-50">
-                        {/* 👇 Render cart item component here */}
-                        <CartItem cart={cart}
+                    // <div className="absolute flex gap-10 right-0 mt-2 max-w-[700px] overflow-clip bg-yellow-100 shadow-lg rounded p-5 z-50">
+                    //     {/* 👇 Render cart item component here */}
+                    //     <CartItem cart={cart}
+                    //         decreaseQuantity={decreaseQuantity}
+                    //         increaseQuantity={increaseQuantity}
+                    //         totalAmount={totalAmount} />
+                    // </div>
+                    <div className="fixed top-16 right-5 w-full max-w-[240px] hide-scrollbar max-h-[80vh] overflow-y-auto bg-yellow-100 shadow-lg rounded p-5 z-50">
+                        <CartItem
+                            cart={cart}
                             decreaseQuantity={decreaseQuantity}
                             increaseQuantity={increaseQuantity}
-                            totalAmount={totalAmount} />
+                            totalAmount={totalAmount}
+                        />
                     </div>
+
                 )}
 
             </div>
