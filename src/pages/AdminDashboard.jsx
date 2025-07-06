401d1d096cb5686d51e668947f35b0f243ebe4e3
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-06 14:13:07 +0530

    ...

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AdminDashboard() {
+function AdminDashboard({onAddProduct}) {
+
+    const [title,setTitle] = useState('')
+    const [image,setImage] = useState('')
+    const [price,setPrice] = useState('')
+    const [description,setDescription] = useState('')
+
+    
   return (
     <div>AdminDashboard</div>
   )
