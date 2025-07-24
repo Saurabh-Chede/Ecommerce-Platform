fd31d1936ef5a2fae9a8b175e2dda9d6e9ab7770
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-24 11:19:44 +0530

    ...

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,4 @@
-
-function Pagination({totalPages,currentPage,setCurrentPage}) {
+function Pagination({ totalPages, currentPage, setCurrentPage }) {
     return (
         <div className="flex justify-center mt-6 gap-2 flex-wrap mb-6">
             {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNum) => (
@@ -7,8 +6,8 @@ function Pagination({totalPages,currentPage,setCurrentPage}) {
                     key={pageNum}
                     onClick={() => setCurrentPage(pageNum)}
                     className={`px-4 py-2 rounded border ${currentPage === pageNum
-                            ? "bg-black text-white font-semibold"
-                            : "bg-gray-100 text-black hover:bg-gray-200"
+                        ? "bg-black text-white font-semibold"
+                        : "bg-gray-100 text-black hover:bg-gray-200"
                         }`}
                 >
                     {pageNum}
