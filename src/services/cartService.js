c9b2c516f03926032e7c48f2e3396667451b2124
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-29 11:47:15 +0530

    ...

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,30 +1,3 @@
-
-// import { db } from "../firebase";
-// import { doc, setDoc, getDoc } from "firebase/firestore";
-
-// export const saveCartToFirestore = async (userId, cartItems) => {
-//   try {
-//     await setDoc(doc(db, "carts", userId), { items: cartItems });
-//   } catch (error) {
-//     console.error("Error saving cart:", error);
-//   }
-// };
-
-// export const getCartFromFirestore = async (userId) => {
-//   try {
-//     const docRef = doc(db, "carts", userId);
-//     const docSnap = await getDoc(docRef);
-//     if (docSnap.exists()) {
-//       return docSnap.data().items || [];
-//     } else {
-//       return [];
-//     }
-//   } catch (error) {
-//     console.error("Error fetching cart:", error);
-//     return [];
-//   }
-// };
-
 import { db } from "../firebase";
 import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 
